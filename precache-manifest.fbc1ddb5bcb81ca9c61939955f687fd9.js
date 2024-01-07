11b9fe17a97deeaed450b6f0220285a2ff640b2b
Piyush Dhyani <piyushdhyani7@gmail.com>
2024-01-07 16:47:19 +0530

    Updates

---
+++ precache-manifest.fbc1ddb5bcb81ca9c61939955f687fd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3562df62fb903fb4ff0e09a8b5aed1",
+    "url": "/Textulites/index.html"
+  },
+  {
+    "revision": "1edb052bd8fe4864a179",
+    "url": "/Textulites/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b19d159b268a21abdf97",
+    "url": "/Textulites/static/js/2.30558358.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Textulites/static/js/2.30558358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf6af97904aebb53983",
+    "url": "/Textulites/static/js/3.93a34e7e.chunk.js"
+  },
+  {
+    "revision": "1edb052bd8fe4864a179",
+    "url": "/Textulites/static/js/main.a285c44d.chunk.js"
+  },
+  {
+    "revision": "baf2dc51e82b84fa4701",
+    "url": "/Textulites/static/js/runtime-main.863a7d1b.js"
+  }
+]);
